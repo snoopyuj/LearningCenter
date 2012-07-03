9255a995d08650980c3ecd3ca3634088e6b65ca2
Andy Wang <litopon@minelab.tw>
2012-07-03 20:10:27 +0800

    Save a temp version for git flow init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '373518166042529', '24a9e3b9694d67b420953e9c514c8de3'
+  provider :facebook, '373518166042529', '24a9e3b9694d67b420953e9c514c8de3', { :scope => 'email'}
+  #provider :twitter, 'DiBlIui2JWbnO06Jzwuaw', 'RYI8TWLquo99ltKZAmc2s1RPwJteXIJPR9ei45b7E'
 end
